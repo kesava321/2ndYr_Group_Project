c71cd50110c35af48b18704a0addf321ba598001
Kesava Uthaya-kumaran <Kesava Uthaya-kumaran>
2016-11-29 17:38:51 +0000

    Added pseudocode for main interface.

--- Code/src/main/main.java
+++ Code/src/main/main.java
@@ -4,3 +4,19 @@ public class main{
 
 }
 //test message aleem
+
+/*Pseudocode
+-Program loads when user clicks the software.jar file
+-Display message appears, which says welcome to building control systems developed by Mr Robot Software Group
+ Please select an energy consumer: 1)Electricity
+                                   2)Water
+                                   3)Heatting
+                                   4)Gas
+ (Once the user selects an option from above it directs them to the next window)
+
+-Please select an option: 1)Control
+                          2)Simulation
+ (Once the user selects an option from above it directs them to the next window)
+
+
+ */
