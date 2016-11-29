04527ad0d85c347b053f620ed1c306fba09c1ce8
Kesava Uthaya-kumaran <Kesava Uthaya-kumaran>
2016-11-29 17:42:12 +0000

    Added pseudcode for sub menu

--- Code/src/main/main.java
+++ Code/src/main/main.java
@@ -18,5 +18,10 @@ public class main{
                           2)Simulation
  (Once the user selects an option from above it directs them to the next window)
 
+-Please select an option: 1)Control
+                          2)Monitor
+                          2)Simulation
 
+Deadline: The above has to be completed by the end of this week, as we only have two weeks left.
+          Also now that OSC and ACE deadline has gone please do as much coding as you can.
  */
