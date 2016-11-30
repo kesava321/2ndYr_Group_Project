51842117740e41243a541f0fc35cfe71bca32638
Daniel Doynov <psydbd@nottingham.ac.uk>
2016-11-30 12:19:24 +0000

    added basic javafx ui

--- src/main/java/main/CAB.java
+++ src/main/java/main/CAB.java
@@ -25,7 +25,7 @@ import javafx.stage.Stage;
 public class CAB extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../../resources/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource(  "sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
