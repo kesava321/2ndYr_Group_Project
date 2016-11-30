8c3872ac6270b71d6b183a9c8896d6463e8b846c
Kesava Uthaya-kumaran <Kesava Uthaya-kumaran>
2016-11-30 14:17:41 +0000

    MainMenu .java, .css and .fxml created

---
+++ src/main/resources/MainMenu.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Kesava on 30/11/2016.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainMenu extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
