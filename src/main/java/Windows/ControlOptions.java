37de528b8f1c838cf753a84274ff645064e959e3
Daniel Doynov <psydbd@nottingham.ac.uk>
2016-12-06 12:05:00 +0000

    send to matt

--- src/main/java/Windows/ControlOptions.java
+++ src/main/java/Windows/ControlOptions.java
@@ -1,10 +1,14 @@
 package Windows;
 
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Created by daniel on 04/12/2016.
@@ -12,8 +16,8 @@ import javafx.stage.Stage;
 public class ControlOptions implements ScreenViewController
 {
     Stage window = new Stage();
-    GridPane grid = new GridPane();
-
+    //GridPane grid = new GridPane();
+    private static Parent root;
     @Override
     public void setScreenParent(ScreensController screenPage)
     {
@@ -21,12 +25,19 @@ public class ControlOptions implements ScreenViewController
     }
     public void start()
     {
+        try
+        {
+            root = FXMLLoader.load(getClass().getResource("ControlOptions.fxml"));
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
         Button test = new Button("TEST");
-        grid.setPadding(new Insets(0,0,0,0));
-        grid.setVgap(0);
-        grid.setHgap(0);
-        grid.setConstraints(test,0,0);
-        Scene scene = new Scene(grid);
+        //grid.setPadding(new Insets(0,0,0,0));
+        //grid.setVgap(0);
+        //grid.setHgap(0);
+        //grid.setConstraints(test,0,0);
+        Scene scene = new Scene(root,600,400);
         window.setScene(scene);
         window.show();
 
