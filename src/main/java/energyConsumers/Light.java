e583f8d054e4a2fffca3ed5a3f3fc245c359c627
Matt Kempa <psymjk@exmail.nottingham.ac.uk>
2016-12-08 13:36:29 +0000

    added light class

--- src/main/java/energyConsumers/Light.java
+++ src/main/java/energyConsumers/Light.java
@@ -6,15 +6,18 @@ package energyConsumers;
 public class Light extends energyConsumers
 {
     private Boolean lightState;
+    private double powerrating;
 
     public Light()
     {
         setLightState(false);
+        setPowerrating(60.0);
     }
 
-    public Light(Boolean state)
+    public Light(Boolean state, double powerrating)
     {
         setLightState(state);
+        setPowerrating(powerrating);
     }
 
     public Boolean getLightState()
@@ -25,5 +28,16 @@ public class Light extends energyConsumers
     public void setLightState(Boolean lightState)
     {
         this.lightState = lightState;
+    }
+
+    public void setPowerrating(double powerrating) {
+        if(powerrating>0)
+            this.powerrating = powerrating;
+        else
+            this.powerrating =0;
+    }
+
+    public double getPowerrating() {
+        return powerrating;
     }
 }
