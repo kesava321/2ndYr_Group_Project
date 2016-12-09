c8b2e66e2b2e6d252fce677551d6c78f1a0d12fd
Matt Kempa <psymjk@exmail.nottingham.ac.uk>
2016-12-09 14:35:13 +0000

    Added JUnit tests for heating - testing bounds

--- src/test/JUnit_Tests/energyConsumers/HeatingTest.java
+++ src/test/JUnit_Tests/energyConsumers/HeatingTest.java
@@ -21,6 +21,7 @@ public class HeatingTest {
     }
 
     @Test
+    /** A test for the desfault constructor */
     public void getTemperatureDefaultConst() throws Exception {
         Heating heater = new Heating();
         double x = heater.getTemperature();
@@ -28,8 +29,46 @@ public class HeatingTest {
     }
 
     @Test
-    public void setTemperature() throws Exception {
+    /** A test for checking the constructor with values passed of 20 and 10000 */
+    public void getTemperatureTestingSetter__Temperature() throws Exception {
+        Heating heater = new Heating(20, 10000);
+        double x = heater.getTemperature();
+        assertEquals(20, x, 0.0);
+    }
 
+    @Test
+    /** A test for checking the constructor with values passed of 20 and 10000 - POWER RATING */
+    public void getTemperatureTestingSetter__PowerRating() throws Exception {
+        Heating heater = new Heating(20, 10000);
+        double x = heater.getPowerRating();
+        assertEquals(10000, x, 0.0);
+    }
+
+    @Test
+    /** Testing a valid tempature entry */
+    public void setTemperature__VALID() throws Exception {
+        Heating heater = new Heating();
+        heater.setTemperature(20);
+        double x = heater.getTemperature();
+        assertEquals(20, x, 0.0);
+    }
+
+    @Test
+    /** Testing a invalid tempature entry -- Too high */
+    public void setTemperature__INVALID_High() throws Exception {
+        Heating heater = new Heating();
+        heater.setTemperature(41);  //should reduce to 40
+        double x = heater.getTemperature();
+        assertEquals(40, x, 0.0);
+    }
+
+    @Test
+    /** Testing a valid tempature entry -- Too low */
+    public void setTemperature__INVALID_Low() throws Exception {
+        Heating heater = new Heating();
+        heater.setTemperature(-2);
+        double x = heater.getTemperature();
+        assertEquals(0, x, 0.0);
     }
 
     @Test
