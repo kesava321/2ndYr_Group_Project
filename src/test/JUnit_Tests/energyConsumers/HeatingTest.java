120b7f0eafe184d2239e585a8c30726830bef154
Matt Kempa <psymjk@exmail.nottingham.ac.uk>
2016-12-09 13:33:34 +0000

    Wrote Junit tests - sending to Dan to sort gradle

---
+++ src/test/JUnit_Tests/energyConsumers/HeatingTest.java
@@ -0,0 +1,45 @@
+package energyConsumers;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by User on 09-Dec-16.
+ */
+public class HeatingTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getTemperatureDefaultConst() throws Exception {
+        Heating heater = new Heating();
+        double x = heater.getTemperature();
+        assertEquals(0, x, 0.0);
+    }
+
+    @Test
+    public void setTemperature() throws Exception {
+
+    }
+
+    @Test
+    public void getPowerRating() throws Exception {
+
+    }
+
+    @Test
+    public void setPowerRating() throws Exception {
+
+    }
+
+}
