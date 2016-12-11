e7e3995a172aa42a5d0ae0de07de5d445cb7903a
Daniel Doynov <psydbd@nottingham.ac.uk>
2016-12-11 11:44:15 +0000

    modified .gitigmore to remove .DS_Store

--- src/test/java/HeatingTest.java
+++ src/test/java/HeatingTest.java
@@ -3,6 +3,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.Before;
 import static org.junit.Assert.*;
 
 /**
