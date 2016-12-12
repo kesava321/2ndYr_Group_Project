041138d922c563a53252eaaa96574e280db498d4
Kesava Uthaya-kumaran <Kesava Uthaya-kumaran>
2016-12-12 20:41:42 +0000

    Added WaterState varibles and setters

--- src/main/java/energyConsumers/Water.java
+++ src/main/java/energyConsumers/Water.java
@@ -3,5 +3,32 @@ package energyConsumers;
 /**
  * Created by Kesava on 12/12/2016.
  */
-public class Water {
+    public class Water extends energyConsumers
+    {
+        private Boolean WaterState; //Hot/Cold
+        private double PowerRating; //Watts
+
+
+
+    public Water()
+    {
+        setWaterState(false);
+        setPowerRating(50.0);
+    }
+
+    public Water(Boolean state, double PowerRating)
+    {
+        setWaterState(state);
+        setPowerRating(PowerRating);
+    }
+
+
+    public void setWaterState(boolean waterState) {
+        this.WaterState = waterState;
+    }
+
+    public void setPowerRating(double PowerRating) {
+        this.PowerRating = PowerRating;
+    }
 }
+ 
