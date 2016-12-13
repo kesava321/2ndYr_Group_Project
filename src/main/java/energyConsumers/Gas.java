b602e1ba110587cf36fafe6de67ff60b80c4c574
Kesava Uthaya-kumaran <Kesava Uthaya-kumaran>
2016-12-13 15:47:27 +0000

    Added GasState variables and setters

--- src/main/java/energyConsumers/Gas.java
+++ src/main/java/energyConsumers/Gas.java
@@ -4,4 +4,30 @@ package energyConsumers;
  * Created by Kesava on 13/12/2016.
  */
 public class Gas extends energyConsumers {
+
+    private Boolean GasState;
+    private double powerRating; //Watts
+    //Number of boilers
+    //Number of kitchens
+
+
+    public Gas()
+    {
+        setGasState(false);
+        setPowerRating(70.0);
+    }
+
+    public Gas(Boolean state, double powerRating)
+    {
+        setGasState(state);
+        setPowerRating(powerRating);
+    }
+
+    public void setGasState(boolean gasState) {
+        this.GasState = gasState;
+    }
+
+    public void setPowerRating(double powerRating) {
+        this.powerRating = powerRating;
+    }
 }
