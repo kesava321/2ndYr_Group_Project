72ed5d919c74228942d8024730f70650b7bbd765
Matt Kempa <psymjk@exmail.nottingham.ac.uk>
2016-12-15 21:06:20 +0000

    Completed main classes JUnits

--- src/test/java/energyConsumers/GasTest.java
+++ src/test/java/energyConsumers/GasTest.java
@@ -5,26 +5,36 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Created by User on 13-Dec-16.
+ * Created by Matt on 13-Dec-16.
  */
 public class GasTest {
     @Test
     public void getGasState() throws Exception {
-
+        Gas consumer = new Gas();
+        boolean x = consumer.getGasState();
+        assertEquals(x, false);
     }
 
     @Test
     public void setGasState() throws Exception {
-
+        Gas consumer = new Gas();
+        consumer.setGasState(true);
+        boolean x = consumer.getGasState();
+        assertEquals(x, true);
     }
 
     @Test
     public void getGasUsage() throws Exception {
-
+        Gas consumer = new Gas();
+        double x = consumer.getGasUsage();
+        assertEquals(x, 70,0);
     }
 
     @Test
     public void setGasUsage() throws Exception {
-
+        Gas consumer = new Gas();
+        consumer.setGasUsage(110);
+        double x = consumer.getGasUsage();
+        assertEquals(x, 110, 0);
     }
 }
