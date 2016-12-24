db51ec690b8ffb7f5daa5389ffcf6effb69f5e0e
Daniel Doynov <psydbd@nottingham.ac.uk>
2016-12-24 22:58:27 +0000

    partially working drag ui

--- src/main/java/Windows/CreateRoom.java
+++ src/main/java/Windows/CreateRoom.java
@@ -1,11 +1,22 @@
 package Windows;
 
+import energyConsumers.Light;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Stack;
 
 /**
  * Created by daniel on 24/12/2016.
@@ -13,16 +24,17 @@ import javafx.stage.Stage;
 public class CreateRoom
 {
     private Stage window = new Stage();
-    public /*static*/ TabPane tabPanel;
-
+    public TabPane tabPanel;
+    StackPane canvas = new StackPane();
+    public ArrayList<Light> lights = new ArrayList<Light>();
     private ToolBar toolbar = new ToolBar(
             new Button("Mouse"),
             new Button("Pen")
     );
 
+        BorderPane root = new BorderPane();
     private BorderPane build()
     {
-        BorderPane root = new BorderPane();
         tabPanel = new TabPane();
         root.setCenter(tabPanel);
         Accordion accordion = new Accordion();
@@ -40,25 +52,39 @@ public class CreateRoom
     public void start() throws Exception
     {
         BorderPane borderPane = new BorderPane(build());
+        ListView list = new ListView();
+        BorderPane.setAlignment(list, Pos.TOP_LEFT);
+        BorderPane.setMargin(list, new Insets(12,12,12,12));
         borderPane.setTop(toolbar);
+        //borderPane.setCenter(canvas);
+        //drawLight();
         Scene scene = new Scene(borderPane,800,600);
         window.setScene(scene);
         window.show();
     }
 
-
-
-}
-class Lights {
-
-    public Pane getView() {
-        Pane p = new Pane();
-        Button button = new Button("LED Bulb");
-        Button button1 = new Button("Incandescent"); //probs should change to image view at a later date
-        VBox vBox = new VBox(5);
-        vBox.getChildren().addAll(button,button1);
-        p.getChildren().addAll(vBox);
-        return p;
+    private void drawLight()
+    {
+        File file = new File("Images/bulb.png");
+        Image image = new Image(file.toURI().toString());
+        ImageView imageView = new ImageView(image);
+        //canvas.getChildren().add(imageView);
+        root.setCenter(imageView);
     }
 
+    class Lights
+    {
+
+        public Pane getView() {
+            Pane p = new Pane();
+            Button button = new Button("LED Bulb"); //probs should change to image view at a later date
+        button.setOnMouseClicked(event ->
+                lights.add(new Light(false,100)));
+            VBox vBox = new VBox(5);
+            vBox.getChildren().addAll(button);
+            p.getChildren().addAll(vBox);
+            return p;
+        }
+
+    }
 }
