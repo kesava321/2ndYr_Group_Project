ecf1f030fcca8889229704cd9bbcee758953a4d0
Kesava Uthaya-kumaran <Kesava Uthaya-kumaran>
2017-01-30 21:48:22 +0000

    Created energyData class to read and parse data from file

---
+++ src/main/java/main/energyData.java
@@ -0,0 +1,30 @@
+package main;
+
+/**
+ * Created by Kesava on 30/01/2017.
+ */
+public class energyData {
+
+
+    private String energyConsumer;
+    private int quantity;
+    private float energyRating;
+
+    public energyData(String energyConsumer, int quantity, float energyRating)
+    {
+        this.energyConsumer = energyConsumer;
+        this.quantity = quantity;
+        this.energyRating = energyRating;
+    }
+
+    public float getTotal()
+    {
+        return quantity * energyRating;
+    }
+
+    public String toString()
+    {
+        return "Energy Consumer: " + energyConsumer + "\n" + "Quantity: " + quantity + "\n"
+                + "Energy Rating: " + energyRating;
+    }
+}
