0155fe81c49a2efb55bb5366eba42384c52b916e
Matt Kempa <psymjk@exmail.nottingham.ac.uk>
2017-01-30 13:39:38 +0000

    Fixed light, heating, gas heating unit tests

--- src/test/java/energyConsumers/LightTest.java
+++ src/test/java/energyConsumers/LightTest.java
@@ -1,57 +1,62 @@
-/*
+package energyConsumers;/*
 package energyConsumers;
 
 
 import static org.junit.Assert.*;
 
 */
+
+import energyConsumers.Light;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Matt on 11-Dec-16.
- *//*
+ */
 
 public class LightTest {
     @org.junit.Test
     public void getLightState__DEFAULTCONSTRUCTOR() throws Exception {
         Light lights = new Light();
-        boolean x = lights.getLightState();
+        boolean x = lights.getState();
         assertEquals(x,false);
     }
 
     @org.junit.Test
     public void getLightState__CONSTRUCTOR() throws Exception {
         Light lights = new Light(true, 500);
-        boolean x = lights.getLightState();
+        boolean x = lights.getState();
         assertEquals(x,true);
     }
 
     @org.junit.Test
     public void setLightState() throws Exception {
         Light lights = new Light(false, 500);
-        lights.setLightState(true);
-        boolean x = lights.getLightState();
+        lights.setState(true);
+        boolean x = lights.getState();
         assertEquals(x,true);
     }
 
     @org.junit.Test
     public void setPowerrating() throws Exception {
         Light lights = new Light(false, 500);
-        lights.setPowerrating(400);
-        double x = lights.getPowerrating();
+        lights.setMaxPower(400);
+        double x = lights.getUsage();
         assertEquals(x,400,0.0);
     }
 
     @org.junit.Test
     public void getPowerRating__DEFAULTCONSTRUCTOR() throws Exception {
         Light lights = new Light();
-        double x = lights.getPowerrating();
+        double x = lights.getUsage();
         assertEquals(x,60, 0.0);
     }
 
     @org.junit.Test
     public void getPowerRating__CONSTRUCTOR() throws Exception {
         Light lights = new Light(true, 500);
-        double x = lights.getPowerrating();
+        double x = lights.getUsage();
         assertEquals(x,500, 0.0);
     }
 
-}*/
+}
--- src/test/java/energyConsumers/energyConsumersTest.java
+++ src/test/java/energyConsumers/energyConsumersTest.java
@@ -1,4 +1,4 @@
-/*
+package energyConsumers;/*
 package energyConsumers;
 
 import org.junit.Test;
@@ -6,22 +6,28 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 */
+
+import energyConsumers.Light;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Matt on 11-Dec-16.
- *//*
+ */
 
 public class energyConsumersTest {
     @Test
     public void powerConsumption() throws Exception {
         Light lights = new Light(true, 400);
-        double x = lights.powerConsumption(90, lights.getPowerrating());
+        double x = lights.getConsumption(90);
         assertEquals(600,x,0.0);
     }
 
     @Test
     public void calculateCost() throws Exception {
         Light lights = new Light(true, 400);
-        double x = lights.powerConsumption(90, lights.getPowerrating());
+        double x = lights.getConsumption(90);
         double y = lights.calculateCost(x, 2);
         assertEquals(1.2,y,0.0);
     }
@@ -29,9 +35,9 @@ public class energyConsumersTest {
     @Test
     public void estimatedEmissions() throws Exception {
         Light lights = new Light(true, 400);
-        double x = lights.powerConsumption(90, lights.getPowerrating());
-        double y = lights.estimatedEmissions(x);
+        double x = lights.getConsumption(90);
+        double y = lights.estimatedEmissions((int) x);
         assertEquals(316.2,y,0.0);
     }
 
-}*/
+}
