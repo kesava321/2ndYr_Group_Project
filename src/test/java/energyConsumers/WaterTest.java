f8b4579b6dabb2a8bc1ad74ef9a71582583dabb8
Matt Kempa <psymjk@exmail.nottingham.ac.uk>
2017-01-30 13:40:58 +0000

    Fixed Water JUnit test

--- src/test/java/energyConsumers/WaterTest.java
+++ src/test/java/energyConsumers/WaterTest.java
@@ -1,4 +1,4 @@
-/*
+package energyConsumers;/*
 package energyConsumers;
 
 import org.junit.Test;
@@ -6,39 +6,45 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 */
+
+import energyConsumers.Water;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Matt on 15-Dec-16.
- *//*
+ */
 
 public class WaterTest {
     @Test
     public void getWaterState() throws Exception {
         Water source = new Water();
-        boolean x = source.getWaterState();
+        boolean x = source.getState();
         assertEquals(x,false);
     }
 
     @Test
     public void setWaterState() throws Exception {
         Water source = new Water();
-        source.setWaterState(true);
-        boolean x = source.getWaterState();
+        source.setState(true);
+        boolean x = source.getState();
         assertEquals(x,true);
     }
 
     @Test
     public void getWaterIntake() throws Exception {
         Water source = new Water();
-        double x = source.getWaterIntake();
+        double x = source.getUsage();
         assertEquals(x,50.0,0);
     }
 
     @Test
     public void setWaterIntake() throws Exception {
         Water source = new Water();
-        source.setWaterIntake(75);
-        double x = source.getWaterIntake();
+        source.setUsage(75);
+        double x = source.getUsage();
         assertEquals(x,75,0);
     }
 
-}*/
+}
