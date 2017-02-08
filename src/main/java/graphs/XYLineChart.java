825719c897a251301829e0c84622ca24d36045d5
Kesava Uthaya-kumaran <Kesava Uthaya-kumaran>
2017-02-08 23:03:10 +0000

    Fixed compilation errors by adding jfreechart dependancy

--- src/main/java/graphs/XYLineChart.java
+++ src/main/java/graphs/XYLineChart.java
@@ -1,22 +1,20 @@
 package graphs;
 
 //commented until library can be added to gradle
-/*
+//*
 import java.awt.Color;
 import java.awt.BasicStroke;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.plot.PlotOrientation;
-
+import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.ui.RefineryUtilities;
-
 
 class XYLineChart_AWT extends ApplicationFrame
 {
@@ -44,9 +42,6 @@ class XYLineChart_AWT extends ApplicationFrame
         plot.setRenderer( renderer );
         setContentPane( chartPanel );
     }
-}
-
-*/
 
     private XYDataset createDataset( )
     {
