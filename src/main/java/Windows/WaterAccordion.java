e2ac3af3ff7a9150a103754cb134a542e825bf24
Kesava Uthaya-kumaran <Kesava Uthaya-kumaran>
2017-02-15 19:14:32 +0000

    Added Water Pane and Tap Object

---
+++ src/main/java/Windows/WaterAccordion.java
@@ -0,0 +1,78 @@
+package Windows;
+
+        import energyConsumers.ElectricHeating;
+        import energyConsumers.GasHeating;
+        import energyConsumers.WaterTemp;
+        import javafx.scene.Cursor;
+        import javafx.scene.control.Button;
+        import javafx.scene.image.Image;
+        import javafx.scene.image.ImageView;
+        import javafx.scene.layout.Pane;
+        import javafx.scene.layout.VBox;
+        import org.apache.commons.lang3.BooleanUtils;
+
+        import static Windows.CreateRoom.*;
+/**
+ * Created by Kesava on 15/02/2017.
+ */
+public class WaterAccordion
+{
+
+    private ImageView drawTap()
+    {
+        int id = count;
+        Image image = new Image("Images/waterTap.png",50,50,false,false);
+        ImageView imageView = new ImageView(image);
+        imageView.setCursor(Cursor.HAND);
+        imageView.setOnMousePressed(event ->
+        {
+            currentSelected = id;
+            waterPreferences.setVisible(true);
+            heatPreferences.setVisible(false);
+            lightPreferences.setVisible(false);
+            Object temp = energyConsumers.get(id);
+            if(temp instanceof WaterTemp)
+            {
+                waterPreferences.stateCombo.getSelectionModel().select(BooleanUtils.toInteger(((WaterTemp) temp).getState()));
+                waterPreferences.powerRatingField.setText(Double.toString(((WaterTemp) temp).getUsage()));
+                waterPreferences.tempField.setText(Double.toString(((WaterTemp) temp).getTemperature()));
+            }
+            else
+                System.out.println("oh-oh");
+            orgSceneX = event.getSceneX();
+            orgSceneY = event.getSceneY();
+        });
+        imageView.setOnMouseDragged(event ->
+        {
+            if((event.getX() >0 && event.getX() < canvas.getWidth()) && (event.getY() > 0 && event.getY() < canvas.getHeight()))
+            {
+                double offsetX = event.getSceneX() - orgSceneX;
+                double offsetY = event.getSceneY() - orgSceneY;
+                Object o = event.getSource();
+                ImageView i = (ImageView) o;
+                i.setX(i.getX() + offsetX);
+                i.setY(i.getY() + offsetY);
+                orgSceneX = event.getSceneX();
+                orgSceneY = event.getSceneY();
+            }
+        });
+        return imageView;
+    }
+
+    public Pane getView()
+    {
+        Pane p = new Pane();
+        Button waterTap = new Button("Tap");
+        waterTap.setOnMouseClicked(event ->
+        {
+            ImageView image = drawTap();
+            canvas.getChildren().add(image);
+            energyConsumers.add(new WaterTemp(true,100,25));
+            count++;
+        });
+        VBox vBox = new VBox(5);
+        vBox.getChildren().addAll(waterTap);
+        p.getChildren().add(vBox);
+        return p;
+    }
+}
---
+++ src/main/java/Windows/WaterPreferences.java
@@ -0,0 +1,103 @@
+package Windows;
+
+        import energyConsumers.ElectricHeating;
+        import energyConsumers.Gas;
+        import energyConsumers.GasHeating;
+        import javafx.scene.control.ComboBox;
+        import javafx.scene.control.Label;
+        import javafx.scene.control.TextField;
+        import javafx.scene.layout.GridPane;
+        import javafx.scene.layout.Pane;
+        import org.apache.commons.lang3.BooleanUtils;
+
+        import static Windows.CreateRoom.currentSelected;
+        import static Windows.CreateRoom.update;
+
+
+/**
+ * Created by Kesava on 15/02/2017.
+ */
+class WaterPreferences
+{
+    Label state = new Label("Water State");
+    ComboBox stateCombo = new ComboBox();
+    Pane pane = new Pane();
+    Label temp = new Label("Temperature");
+    TextField tempField = new TextField();
+    Label powerRating = new Label("Power Rating");
+    TextField powerRatingField = new TextField();
+    GridPane grid = new GridPane();
+
+    public void setVisible(boolean visible)
+    {
+        pane.setVisible(visible);
+    }
+
+    public Pane init()
+    {
+        stateCombo.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) ->
+        {
+            Object tempObject = CreateRoom.energyConsumers.get(currentSelected);
+            if(tempObject instanceof ElectricHeating)
+            {
+                ((ElectricHeating) tempObject).setState(BooleanUtils.toBoolean(stateCombo.getSelectionModel().getSelectedIndex()));
+                update();
+            }
+            else if(tempObject instanceof GasHeating)
+            {
+                ((GasHeating) tempObject).setState(BooleanUtils.toBoolean(stateCombo.getSelectionModel().getSelectedIndex()));
+                update();
+            }
+        });
+        powerRatingField.textProperty().addListener((observable, oldValue, newValue) ->
+        {
+            Object tempObject = CreateRoom.energyConsumers.get(currentSelected);
+            if (!Validation.Validate.vDouble(newValue))
+                powerRatingField.getStyleClass().add("error");
+            else
+            {
+                powerRatingField.getStyleClass().remove("error");
+                if(tempObject instanceof ElectricHeating)
+                {
+                    ((ElectricHeating) tempObject).setUsage(Double.parseDouble(newValue));
+                    update();
+                }
+                else if(tempObject instanceof GasHeating)
+                {
+                    ((GasHeating) tempObject).setUsage(Double.parseDouble(newValue));
+                    update();
+                }
+            }
+        });
+        tempField.textProperty().addListener((observable, oldValue, newValue) ->
+        {
+            Object tempObject = CreateRoom.energyConsumers.get(currentSelected);
+            if (!Validation.Validate.vDouble(newValue))
+                tempField.getStyleClass().add("error");
+            else
+            {
+                if(tempObject instanceof ElectricHeating)
+                {
+                    tempField.getStyleClass().remove("error");
+                    ((ElectricHeating) tempObject).setTemperature(Double.parseDouble(newValue));
+                    update();
+                }
+                else if(tempObject instanceof GasHeating)
+                {
+                    ((GasHeating) tempObject).setTemperature(Double.parseDouble(newValue));
+                    update();
+                }
+            }
+        });
+        stateCombo.getItems().addAll("Off", "On");
+        grid.setConstraints(state, 0, 0);
+        grid.setConstraints(stateCombo, 1, 0);
+        grid.setConstraints(temp, 0, 1);
+        grid.setConstraints(tempField, 1, 1);
+        grid.setConstraints(powerRating, 0, 2);
+        grid.setConstraints(powerRatingField, 1, 2);
+        grid.getChildren().addAll(state, stateCombo, temp, tempField, powerRating, powerRatingField);
+        pane.getChildren().add(grid);
+        return pane;
+    }
+}
