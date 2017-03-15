4801f96011af0b46eb6c3b4163e42a73eb4e2f7f
Daniel Doynov <psydbd@nottingham.ac.uk>
2017-03-15 13:18:59 +0000

    fixed slight water bug

--- src/main/java/Windows/RoomSettings.java
+++ src/main/java/Windows/RoomSettings.java
@@ -7,7 +7,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-import org.apache.commons.lang3.BooleanUtils;
 
 /**
  * Created by daniel on 15/03/2017.
