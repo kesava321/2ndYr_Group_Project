94532d73e7806665adb8f7acf1bc3e68d72b3001
Matt Kempa <psymjk@exmail.nottingham.ac.uk>
2017-03-15 23:52:40 +0000

    minor cleanup of splash screen - still not working

--- src/main/java/main/splash.java
+++ src/main/java/main/splash.java
@@ -1,32 +1,51 @@
 package main;
 
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Timer;
+
+import static sun.management.snmp.jvminstr.JvmThreadInstanceEntryImpl.ThreadStateMap.Byte0.runnable;
 
 /**
  * Created by Matt on 24-Dec-16.
  */
-public class splash {
+public class splash implements Runnable{
 
-    private Stage window = new Stage();
+    //private Stage window = new Stage();
 
-    public void Splash() throws IOException {
-
+    public splash() throws IOException, InterruptedException {
+        Stage window = new Stage();
         window.setTitle("Building Control Systems");
-        BorderPane root = new BorderPane();
+        Group root = new Group();
         Scene scene = new Scene(root, 800, 600);
-        root.setStyle("-fx-background-image: url(menu_bgr.jpg);-fx-background-size: 800, 600;-fx-background-repeat: no-repeat;");
+        // load the image
+        Image image = new Image("splash.jpg");
+
+
         window.setScene(scene);
         window.show();
+
+        window.hide();
+    }
+
+
+    @Override
+    public void run() {
         try {
-            Thread.sleep(1000);
+            new splash();
+        } catch (IOException e) {
+            e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        window.hide();
     }
 }
 
